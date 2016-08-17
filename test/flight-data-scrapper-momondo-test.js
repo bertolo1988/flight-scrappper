8ca14b8177339ea98420d15c8277768e76979232
bertolo1988 <tiagobertolo@gmail.com>
2016-08-17 20:03:08 +0100

    Fixed tests

--- test/flight-data-scrapper-momondo-test.js
+++ test/flight-data-scrapper-momondo-test.js
@@ -8,8 +8,18 @@ describe('FlightScrapper tests', function() {
 
     this.timeout(Config.TIMEOUT);
 
-    it('should see data in the database', function(done) {
-        FlightScrapper.run();
+    it('should retrieve 15 results', function(done) {
+        FlightScrapper.run().then(function(resp) {
+            resp.should.be.exactly(15);
+            done();
+        });
+    });
+
+    it('should get "No results" error', function(done) {
+        FlightScrapper.run(["to=PHI"]).then({}, function(err) {
+            (err instanceof Error).should.be.true();
+            done();
+        });
     });
 
 });
