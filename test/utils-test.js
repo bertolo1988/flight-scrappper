bc9aacb05a651317e490744e2256e31f487b2913
bertolo1988 <tiagobertolo@gmail.com>
2016-08-17 17:21:26 +0100

    Fix retrieveFlightDatesArray test

--- test/utils-test.js
+++ test/utils-test.js
@@ -3,9 +3,9 @@ var Moment = require('moment');
 var Utils = require('../src/utils');
 var Config = require('../config');
 
-describe('Utils tests', function() {
 
-    it('should generate 3 dates, in days 7, 9, 11-09-2015', function(done) {
+describe('retrieveFlightDatesArray test', function() {
+    it('should generate 3 dates, in days 7, 9, 11-09-2015 from a moment', function(done) {
         let moment = new Moment('05-09-2015', Config.DATE_FORMAT);
         let dates = Utils.retrieveFlightDatesArray(moment, 3, 48);
         (dates[0]).should.be.equal('07-09-2015');
@@ -13,14 +13,12 @@ describe('Utils tests', function() {
         (dates[2]).should.be.equal('11-09-2015');
         done();
     });
-
-    it('should generate 45 dates from a ISO string', function(done) {
-        var todayDate = new Date().toISOString();
-        var dates = Utils.retrieveFlightDatesArray(todayDate, 45, 24);
+    it('should generate 45 dates from a string', function(done) {
+        let todayDate = new Moment(new Date().toISOString()).format(Config.DATE_FORMAT);
+        let dates = Utils.retrieveFlightDatesArray(todayDate, 45, 24);
         (dates.length).should.be.exactly(45);
-        var todayMoment = new Moment().add(45, 'days').format(Config.DATE_FORMAT);
+        let todayMoment = new Moment().add(45, 'days').format(Config.DATE_FORMAT);
         todayMoment.should.be.equal(dates[44]);
         done();
     });
-
 });
