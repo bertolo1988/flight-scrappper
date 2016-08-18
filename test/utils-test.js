9fd4681b2483a5bb85a8b4eef329a28999aaaf31
bertolo1988 <tiagobertolo@gmail.com>
2016-08-18 23:57:44 +0100

    fix tests due to recent changes on way dates are retrieved

--- test/utils-test.js
+++ test/utils-test.js
@@ -7,16 +7,16 @@ describe('retrieveFlightDatesArray test', () => {
   it('should generate 3 dates, in days 7, 9, 11-09-2015 from a moment', (done) => {
     let moment = new Moment('05-09-2015', Config.DATE_FORMAT);
     let dates = Utils.retrieveFlightDatesArray(moment, 3, 48);
-    (dates[0]).should.be.equal('07-09-2015');
-    (dates[1]).should.be.equal('09-09-2015');
-    (dates[2]).should.be.equal('11-09-2015');
+    (dates[0]).should.be.equal('05-09-2015');
+    (dates[1]).should.be.equal('07-09-2015');
+    (dates[2]).should.be.equal('09-09-2015');
     done();
   });
   it('should generate 45 dates from a string', (done) => {
     let todayDate = new Moment(new Date().toISOString()).format(Config.DATE_FORMAT);
     let dates = Utils.retrieveFlightDatesArray(todayDate, 45, 24);
     (dates.length).should.be.exactly(45);
-    let todayMoment = new Moment().add(45, 'days').format(Config.DATE_FORMAT);
+    let todayMoment = new Moment().add(44, 'days').format(Config.DATE_FORMAT);
     todayMoment.should.be.equal(dates[44]);
     done();
   });
