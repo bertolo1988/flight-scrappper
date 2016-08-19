696bd318a73abef47ef6207470baec29ec5a76d4
bertolo1988 <tiagobertolo@gmail.com>
2016-08-19 14:34:28 +0100

    small improvement in tests

--- test/momondo-test.js
+++ test/momondo-test.js
@@ -3,8 +3,12 @@ var MomondoQueryString = require('../src/momondo-query-string');
 
 describe('momondo-query-string test', () => {
 	it('should generate a good momondo-query-string', (done) => {
-		let query = new MomondoQueryString('LON', 'LON', '23-05-2016', 'USD').toString();
+		let query = new MomondoQueryString('LON', 'PAR', '23-05-2016', 'USD').toString();
 		should.exist(query);
+		(query.includes('USD')).should.be.true();
+		(query.includes('23-05-2016')).should.be.true();
+		(query.includes('LON')).should.be.true();
+		(query.includes('PAR')).should.be.true();
 		var attributes = query.split('&');
 		(attributes.length).should.be.exactly(11);
 		done();
