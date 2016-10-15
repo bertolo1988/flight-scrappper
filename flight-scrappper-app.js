76fe2054c8933e5ebdc83c54e7257706e18e5a6a
Tiago Bértolo <tiagobertolo@gmail.com>
2016-10-15 16:05:09 +0100

    options.directflight is now a boolean

--- flight-scrappper-app.js
+++ flight-scrappper-app.js
@@ -4,60 +4,60 @@ let Utils = require('./src/utils');
 
 var defaultDateFormat = 'DD-MM-YYYY';
 var options = {
-	periods: 15,
-	interval: 96,
-	routes: [{
-		from: 'LIS',
-		to: 'SHA'
-	}, {
-		from: 'LIS',
-		to: 'MAA'
-	}, {
-		from: 'LIS',
-		to: 'TYO'
-	}, {
-		from: 'LIS',
-		to: 'DPS'
-	}, {
-		from: 'LIS',
-		to: 'MLE'
-	}, {
-		from: 'LIS',
-		to: 'LAD'
-	}, {
-		from: 'LIS',
-		to: 'LON'
-	}, {
-		from: 'LIS',
-		to: 'MIL'
-	}, {
-		from: 'LIS',
-		to: 'PAR'
-	}, {
-		from: 'LIS',
-		to: 'BCN'
-	}, {
-		from: 'LIS',
-		to: 'IST'
-	}, {
-		from: 'LIS',
-		to: 'NYC'
-	}, {
-		from: 'LIS',
-		to: 'BER'
-	}],
-	currency: 'EUR',
-	directFlight: 'false',
-	dateFormat: defaultDateFormat,
-	targetDate: Utils.getDefaultDateString(defaultDateFormat),
-	database: 'localhost:27017/flight-scrappper',
-	collection: 'flight-data',
-	timeout: 50000,
-	browser: 'chrome'
+    periods: 10,
+    interval: 96,
+    routes: [{
+        from: 'LIS',
+        to: 'SHA'
+    }, {
+        from: 'LIS',
+        to: 'MAA'
+    }, {
+        from: 'LIS',
+        to: 'TYO'
+    }, {
+        from: 'LIS',
+        to: 'DPS'
+    }, {
+        from: 'LIS',
+        to: 'MLE'
+    }, {
+        from: 'LIS',
+        to: 'LAD'
+    }, {
+        from: 'LIS',
+        to: 'LON'
+    }, {
+        from: 'LIS',
+        to: 'MIL'
+    }, {
+        from: 'LIS',
+        to: 'PAR'
+    }, {
+        from: 'LIS',
+        to: 'BCN'
+    }, {
+        from: 'LIS',
+        to: 'IST'
+    }, {
+        from: 'LIS',
+        to: 'NYC'
+    }, {
+        from: 'LIS',
+        to: 'BER'
+    }],
+    currency: 'EUR',
+    directFlight: false,
+    dateFormat: defaultDateFormat,
+    targetDate: Utils.getDefaultDateString(defaultDateFormat),
+    database: 'localhost:27017/flight-scrappper',
+    collection: 'flight-data',
+    timeout: 50000,
+    browser: 'chrome'
 };
 
 FlightScrappper.run(options).then((value) => {
-	debug('Resolved: ' + Utils.prettifyObject(value.length) + ' flights!');
+    debug('Resolved: ' + Utils.prettifyObject(value.length) + ' flights!');
 }).catch(function(err) {
-	debug('Found an error! ' + err);
+    debug('Found an error! ' + err);
 });
