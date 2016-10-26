69058345761990516106f1e6f0c07f202f8db231
bertolo1988 <tiagobertolo@gmail.com>
2016-10-27 00:33:17 +0100

    fix tests

--- test/momondo-test.js
+++ test/momondo-test.js
@@ -69,7 +69,8 @@ describe('momondoScrappper test', function() {
         data.duration.should.be.above(0);
         data.stops.should.be.aboveOrEqual(0);
         data.flightClass.should.be.aboveOrEqual(0);
-        data.airline.length.should.be.eqlOrAbove(1).and.should.be.an.Array();
+        data.airline.and.should.be.an.Array();
+        data.airline.length.should.be.aboveOrEqual(1);
 
         data.price.amount.should.be.aboveOrEqual(1);
         data.price.currency.should.not.be.null().and.be.a.String();
