21bd6196d6b2f9bff6f9f90f48ceb674d3071dd6
bertolo1988 <tiagobertolo@gmail.com>
2016-10-27 01:45:45 +0100

    save work. release needs to be prepared

--- src/persistency-module.js
+++ src/persistency-module.js
@@ -5,58 +5,47 @@ function persistencyModule() {
 
     const NO_DATABASE = 'none';
 
+    function getConnection(database) {
+        return MongoClient.connect('mongodb://' + database);
+    }
+
     function insertFlights(database, collection, docs) {
         if (database === NO_DATABASE) {
             return Promise.resolve(docs);
         }
-        return new Promise(function(resolve, reject) {
-            if (docs.length > 0) {
-                MongoClient.connect('mongodb://' + database, function(err, db) {
-                    if (err != null) {
-                        reject(err);
-                    } else {
-                        debug('Successfully connected to ' + database);
-                        db.collection(collection).insertMany(docs, function(err, res) {
-                            if (err != null) {
-                                reject(err);
-                            } else {
-                                debug('Persisted ' + res.insertedIds.length + ' results');
-                                db.close();
-                                debug('Closed connection to ' + database);
-                                resolve(res.insertedIds);
-                            }
-                        });
-                    }
+        if (!docs || docs.length <= 0) {
+            debug('No data to be inserted');
+            return Promise.resolve([]);
+        } else {
+            let connect = getConnection(database);
+            return connect.then((db) => {
+                debug('Successfully connected to ' + database);
+                let insertion = db.collection(collection).insertMany(docs);
+                return insertion.then((res) => {
+                    debug('Persisted ' + res.insertedIds.length + ' results');
+                    db.close();
+                    debug('Closed connection to ' + database);
+                    return res.insertedIds;
                 });
-            } else {
-                debug('No data to be inserted');
-                resolve([]);
-            }
-        });
+            });
+        }
     }
 
     function removeFlights(database, collection, ids) {
         if (database === NO_DATABASE) {
             return Promise.resolve(ids);
         }
-        return new Promise(function(resolve, reject) {
-            MongoClient.connect('mongodb://' + database, function(err, db) {
-                if (err != null) {
-                    reject(err);
-                } else {
-                    db.collection(collection).deleteMany({
-                        '_id': {
-                            '$in': ids
-                        }
-                    }, function(err, res) {
-                        if (err != null) {
-                            reject(err);
-                        } else {
-                            resolve(res.result.n);
-                        }
-                        db.close();
-                    });
+        let connect = getConnection(database);
+        return connect.then((db) => {
+            let target = {
+                '_id': {
+                    '$in': ids
                 }
+            };
+            let removal = db.collection(collection).deleteMany(target);
+            return removal.then((res) => {
+                db.close();
+                return res.result.n;
             });
         });
     }
@@ -66,7 +55,6 @@ function persistencyModule() {
         insertFlights,
         removeFlights
     };
-
 }
 
 module.exports = persistencyModule();
