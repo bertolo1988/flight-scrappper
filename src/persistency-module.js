f7110881c6192cdca4bd1fd956229e017351da8e
bertolo1988 <tiagobertolo@gmail.com>
2016-10-29 17:24:55 +0100

    improve persistency code

--- src/persistency-module.js
+++ src/persistency-module.js
@@ -5,55 +5,54 @@ function persistencyModule() {
 
     const NO_DATABASE = 'none';
 
-    function getConnection(database) {
-        return MongoClient.connect('mongodb://' + database);
-    }
-
-    function insertFlights(database, collection, docs) {
+    function executeAction(database, collection, data, callback) {
         if (database === NO_DATABASE) {
-            return Promise.resolve(docs);
+            return Promise.resolve(data);
         }
-        if (!docs || docs.length <= 0) {
-            debug('No data to be inserted');
+        if (!data || data.length <= 0) {
+            debug('No data to be changed');
             return Promise.resolve([]);
-        } else {
-            let connect = getConnection(database);
-            return connect.then((db) => {
-                debug('Successfully connected to ' + database);
-                let insertion = db.collection(collection).insertMany(docs);
-                return insertion.then((res) => {
-                    debug('Persisted ' + res.insertedIds.length + ' results');
-                    db.close();
-                    debug('Closed connection to ' + database);
-                    return res.insertedIds;
-                });
-            });
         }
+        let connection = MongoClient.connect('mongodb://' + database);
+        return connection.then((db) => {
+            debug('Successfully connected to ' + database);
+            return callback(db, collection, data);
+        }).catch((err) => {
+            debug('Failed to connect to ' + database);
+            return err;
+        });
     }
 
-    function removeFlights(database, collection, ids) {
-        if (database === NO_DATABASE) {
-            return Promise.resolve(ids);
-        }
-        let connect = getConnection(database);
-        return connect.then((db) => {
-            let target = {
-                '_id': {
-                    '$in': ids
-                }
-            };
-            let removal = db.collection(collection).deleteMany(target);
-            return removal.then((res) => {
-                db.close();
-                return res.result.n;
-            });
+    function insertData(db, collection, docs) {
+        let insertion = db.collection(collection).insertMany(docs);
+        return insertion.then((res) => {
+            debug('Persisted ' + res.insertedIds.length + ' results');
+            db.close();
+            return res.insertedIds;
+        });
+    }
+
+    function removeData(db, collection, ids) {
+        let target = {
+            '_id': {
+                '$in': ids
+            }
+        };
+        let removal = db.collection(collection).deleteMany(target);
+        return removal.then((res) => {
+            db.close();
+            return res.result.n;
         });
     }
 
     return {
         NO_DATABASE,
-        insertFlights,
-        removeFlights
+        insertFlights: function(database, collection, docs) {
+            return executeAction(database, collection, docs, insertData);
+        },
+        removeFlights: function(database, collection, ids) {
+            return executeAction(database, collection, ids, removeData);
+        }
     };
 }
 
